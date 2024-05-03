6cbc0e050eda84e39cb2e0d21ab0d3cc4835db9f
Everton617 <everton6943@gmail.com>
2024-05-03 17:53:20 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResultsComponent } from './components/results/results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,BannerComponent,FeaturesComponent,AboutComponent,ResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
